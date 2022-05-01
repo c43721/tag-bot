f959449d29f9dd27d98e39491ba1875c97e76544
c43721 <rlahman.game@gmail.com>
2022-05-01 16:10:42 -0500

    feat: start index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import { Client } from '@skyra/http-framework';
+import { Client, Registry } from "@skyra/http-framework";
 
 const client = new Client({
-	discordPublicKey: process.env.DISCORD_PUBLIC_KEY!
+  discordPublicKey: process.env.DISCORD_PUBLIC_KEY!,
 });
+
+const registry = new Registry({});
 
 // Load all the commands and message component handlers:
 await client.load();
+
+await registry.registerAllCommandsInGuild(process.env.GUILD_ID!);
 
 // Start up the HTTP server;
 await client.listen({ port: 3000 });
